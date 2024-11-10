69e96ac50f1ca05041f9eb9face34abec5ec801c
Devesh <deveshpanwer9990@gmail.com>
2024-11-10 12:16:44 +0530

    view menu anchor

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -7,7 +8,7 @@ const Header = () => {
       <div className="header-contents">
         <h2>Order your favoruite food</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Especially designed just to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-        <button>View Menu</button>
+        <a href='#explore-menu'><button>View Menu</button></a>
       </div>
     </div>
   )
