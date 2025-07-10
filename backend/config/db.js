22ad93be5c9bf9205b780d67bd441fbd9bcd108d
devesh panwar <106608715+rex0dev@users.noreply.github.com>
2025-07-10 14:05:01 +0530

    Update db.js
    removed sensitive data

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://deveshpanwer9990:devesh123@cluster0.ku78h.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('db-connection-link').then(()=>console.log("DB Connected"));
 }
